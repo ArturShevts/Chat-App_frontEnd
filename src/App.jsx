cf38a266913bb0aec22ec3d05c708b20b12f82ad
art <art@pop-os.localdomain>
2021-10-21 00:41:37 -0400

    add build

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import './App.css';
 
 const cookies = new Cookies();
 
-const apiKey = 'qgtk9ttyha7j';
+const apiKey = 'gyc6p2ecydy7';
 const authToken = cookies.get("token");
 
 const client = StreamChat.getInstance(apiKey);
